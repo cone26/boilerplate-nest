4f68cc757fb714986d0c424c2cf72d5d9555684a
cone26 <sight41siyah@gmail.com>
2024-04-22 18:31:47 +0900

    chore: api response entity

--- apps/admin/src/default/admin.controller.ts
+++ apps/admin/src/default/admin.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ResponseEntity } from '@libs/common/network/response-entity';
+import { ApiResponseEntity } from '@libs/common/decorator/api-response-entity';
 
 @Controller()
 export class AdminController {
   @Get('health')
+  @ApiResponseEntity({ summary: 'health check' })
   health(): ResponseEntity<unknown> {
     return ResponseEntity.ok().build();
   }
